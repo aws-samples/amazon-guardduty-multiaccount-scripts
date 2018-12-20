abc2ae93282d0eafedfe53b259a2fc8df2253d5b
Gabriel Soltz <thegaby@gmail.com>
2018-12-20 17:59:55 +0100

    fix identation

--- enableguardduty.py
+++ enableguardduty.py
@@ -276,14 +276,14 @@ if __name__ == '__main__':
                     
                     # Member is part but disabled
                     if member_dict[account] == 'Disabled':
-                    print('Account {account} Error: Disabled'.format(account=account))
-                        master_gd_client.start_monitoring_members(
-                            AccountIds=[
-                                account
-                            ],
-                            DetectorId=master_detector_id
-                        )
-                    print('Account {account} Re-Enabled'.format(account=account))
+                       print('Account {account} Error: Disabled'.format(account=account))
+                           master_gd_client.start_monitoring_members(
+                               AccountIds=[
+                                   account
+                               ],
+                               DetectorId=master_detector_id
+                           )
+                       print('Account {account} Re-Enabled'.format(account=account))
                      
                 else:
                     master_gd_client = master_session.client('guardduty', region_name=aws_region)
